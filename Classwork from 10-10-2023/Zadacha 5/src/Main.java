2d5ebe83ee1e3f481bc17624cec767cb7cad8b8e
Климент Георгиев Тенев <KGTenev20@codingburgas.bg>
2023-10-10 10:08:49 +0300

    Fix the 5th task code

--- Classwork from 10-10-2023/Zadacha 5/src/Main.java
+++ Classwork from 10-10-2023/Zadacha 5/src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(int num1, int num2) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
         System.out.println(num1 + num2);
         System.out.println(num1 - num2);
         System.out.println(num1 * num2);
