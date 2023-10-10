da90da9850fa00807605aa437663262cbfd3b2ba
Климент Георгиев Тенев <KGTenev20@codingburgas.bg>
2023-10-10 10:01:34 +0300

    Add 5th task

---
+++ Classwork from 10-10-2023/Zadacha 5/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(int num1, int num2) {
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        if(num2 != 0)
+        {
+            System.out.println(num1 / num2);
+        }
+
+        if(num2 != 0)
+        {
+            System.out.println(num1 % num2);
+        }
+    }
+}
